56bdd0623a2423fc8545aa01d9e9b0ad798d8f86
Jacek Giedronowicz <jacek.giedronowicz@gmail.com>
2020-01-21 20:10:23 +0100

    Initial commit

---
+++ src/engine/MainClass.java
@@ -0,0 +1,34 @@
+package engine;
+
+import org.apache.lucene.document.Document;
+
+public class MainClass {
+    public static void main(String args[])
+    {
+        String params[] =
+                {
+                        "-index", "src\\\\engine\\\\index",
+                        "-docs", "src\\\\engine\\\\buissness_domains",
+                        "-paging", "5",
+                        "-query", "kobiety"
+                };
+
+//        MyIndexFiles indexFiles = new MyIndexFiles();
+//        indexFiles.main(params);
+        MySearchFiles searchFiles = new MySearchFiles();
+        MyResult myResult = null;
+        try {
+            myResult = searchFiles.main(params);
+
+            System.out.println("myRes for Query: " + myResult.getQuery() );
+            for( Document doc : myResult.getAllPages() )
+            {
+                System.out.println( doc.get("path") );
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
